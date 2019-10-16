29c46102660b2dbe33ab04a86e213d4e560eea91
Patricio Gayol <patricio@termmed.com>
2019-10-16 15:27:52 -0300

    Config project variables moved to config.js

---
+++ config.js
@@ -0,0 +1,13 @@
+var config = { //URU
+    serverUrl: "http://104.131.201.98/api/snomed",
+    edition: "uy-edition",
+    release: "v20190615",
+    releaseName: "UY Edition 20190615",
+    langRefset: ["5641000179103"],
+    statsButtons: 0, // Set to 0, 1 or 2 depending on how many statistics buttons you want to show. E.g: 2 for Cadanada (extension and french translation stats).
+    selectedLanguage: "es_UY",
+    selectedFlag: "img/flags/uy.png",
+    selectedLogo: "img/logo-salud-uy-small.png",
+    copyrightText: "&copy; Salud.uy 2019",
+    bussinesLink: "https://Salud.uy"
+}
