8e685b0d0799fe1db8590d17982e3dd88dcf021f
Patricio Gayol <patricio@termmed.com>
2019-10-16 16:25:05 -0300

    Minor fixes and new config object

--- config.js
+++ config.js
@@ -1,13 +1,28 @@
-var config = { //URU
-    serverUrl: "http://104.131.201.98/api/snomed",
-    edition: "uy-edition",
-    release: "v20190615",
-    releaseName: "UY Edition 20190615",
-    langRefset: ["5641000179103"],
-    statsButtons: 0, // Set to 0, 1 or 2 depending on how many statistics buttons you want to show. E.g: 2 for Cadanada (extension and french translation stats).
-    selectedLanguage: "es_UY",
-    selectedFlag: "img/flags/uy.png",
-    selectedLogo: "img/logo-salud-uy-small.png",
-    copyrightText: "&copy; Salud.uy 2019",
-    bussinesLink: "https://Salud.uy"
+// var config = { //URU
+//     serverUrl: "http://104.131.201.98/api/snomed",
+//     edition: "uy-edition",
+//     release: "v20190615",
+//     releaseName: "UY Edition 20190615",
+//     langRefset: ["5641000179103"],
+//     statsButtons: 0, // Set to 0, 1 or 2 depending on how many statistics buttons you want to show. E.g: 2 for Canada (extension and french translation stats).
+//     selectedLanguage: "es_UY",
+//     selectedFlag: "img/flags/uy.png",
+//     selectedLogo: "img/logo-salud-uy-small.png",
+//     copyrightText: "&copy; Salud.uy 2019",
+//     bussinesLink: "https://Salud.uy"
+// }
+
+
+var config = { //IHMI
+    serverUrl: "http://ihmi.termspace.com/api/snomed",
+    edition: "en-edition",
+    release: "v20180731",
+    releaseName: "AMA Edition 2019-06-30",
+    langRefset: ["900000000000509007"],
+    statsButtons: 0, // Set to 0, 1 or 2 depending on how many statistics buttons you want to show. E.g: 2 for Canada (extension and french translation stats).
+    selectedLanguage: "en_US",
+    selectedFlag: "img/flags/us.png",
+    selectedLogo: "",
+    copyrightText: "&copy; AMA 2019",
+    bussinesLink: ""
 }
