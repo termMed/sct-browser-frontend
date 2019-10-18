3e1db01fa9653c6d5b78960dd0bd0169ad9e5e2a
Patricio Gayol <patricio@termmed.com>
2019-10-18 12:16:16 -0300

    Dynamic buttons on landpage

--- config.js
+++ config.js
@@ -1,28 +1,30 @@
-// var config = { //URU
-//     serverUrl: "http://104.131.201.98/api/snomed",
-//     edition: "uy-edition",
-//     release: "v20190615",
-//     releaseName: "UY Edition 20190615",
-//     langRefset: ["5641000179103"],
-//     statsButtons: 0, // Set to 0, 1 or 2 depending on how many statistics buttons you want to show. E.g: 2 for Canada (extension and french translation stats).
-//     selectedLanguage: "es_UY",
-//     selectedFlag: "img/flags/uy.png",
-//     selectedLogo: "img/logo-salud-uy-small.png",
-//     copyrightText: "&copy; Salud.uy 2019",
-//     bussinesLink: "https://Salud.uy"
-// }
-
-
-var config = { //IHMI
-    serverUrl: "http://ihmi.termspace.com/api/snomed",
-    edition: "en-edition",
-    release: "v20180731",
-    releaseName: "AMA Edition 2019-06-30",
-    langRefset: ["900000000000509007"],
-    statsButtons: 0, // Set to 0, 1 or 2 depending on how many statistics buttons you want to show. E.g: 2 for Canada (extension and french translation stats).
-    selectedLanguage: "en_US",
-    selectedFlag: "img/flags/us.png",
-    selectedLogo: "",
-    copyrightText: "&copy; AMA 2019",
-    bussinesLink: ""
+var config = { //URU
+    serverUrl: "http://104.131.201.98/api/snomed",
+    edition: "uy-edition",
+    release: "v20190615",
+    releaseName: "UY Edition 20190615",
+    langRefset: ["5641000179103"],
+    statsButtons: ["AAAAAA", "BBBBBB"], // URI of each statistics page 
+    statsButtonsText:["Link to Statistics 1", "Link to Statistics 2"], // Description text of each button
+    selectedLanguage: "es_UY", // Selects language on /i18n folder 
+    selectedFlag: "img/flags/uy.png",
+    selectedLogo: "img/logo-salud-uy-small.png",
+    copyrightText: "&copy; Salud.uy 2019",
+    bussinesLink: "https://Salud.uy"
 }
+
+
+// var config = { //IHMI
+//     serverUrl: "http://ihmi.termspace.com/api/snomed",
+//     edition: "en-edition",
+//     release: "v20180731",
+//     releaseName: "AMA Edition 2019-06-30",
+//     langRefset: ["900000000000509007"],
+//     statsButtons: [],
+//     statsButtonsText: [],
+//     selectedLanguage: "en_US",
+//     selectedFlag: "img/flags/us.png",
+//     selectedLogo: "",
+//     copyrightText: "&copy; AMA 2019",
+//     bussinesLink: ""
+// }
