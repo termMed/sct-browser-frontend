6f0a8c1dda84e2cd3b55c22551d7b70b2a36720c
Patricio Gayol <patricio@termmed.com>
2019-10-18 14:27:37 -0300

    Added AMA icon

--- config.js
+++ config.js
@@ -1,32 +1,32 @@
-// var config = { //URU
-//     serverUrl: "http://104.131.201.98/api/snomed",
-//     edition: "uy-edition",
-//     release: "v20190615",
-//     releaseName: "UY Edition 20190615",
-//     langRefset: ["5641000179103"],
-//     statsButtons: ["AAAAAA", "BBBBBB"], // URI of each statistics page 
-//     statsButtonsText:["Link to Statistics 1", "Link to Statistics 2"], // Description text of each button
-//     selectedLanguage: "es_UY", // Selects language on /i18n folder 
-//     selectedFlag: "img/flags/uy.png",
-//     selectedRightLogo: "img/logo-salud-uy-small.png",
-//     selectedLeftLogo: "",
-//     copyrightText: "&copy; Salud.uy 2019",
-//     bussinesLink: "https://Salud.uy"
-// }
-
-
-var config = { //IHMI
-    serverUrl: "http://ihmi.termspace.com/api/snomed",
-    edition: "en-edition",
-    release: "v20180731",
-    releaseName: "AMA Edition 2019-06-30",
-    langRefset: ["900000000000509007"],
-    statsButtons: [],
-    statsButtonsText: [],
-    selectedLanguage: "en_US",
-    selectedFlag: "img/flags/us.png",
-    selectedRightLogo: "",
-    selectedLeftLogo:"img/ama-logo.png",
-    copyrightText: "&copy; AMA 2019",
-    bussinesLink: ""
+var config = { //URU
+    serverUrl: "http://104.131.201.98/api/snomed",
+    edition: "uy-edition",
+    release: "v20190615",
+    releaseName: "UY Edition 20190615",
+    langRefset: ["5641000179103"],
+    statsButtons: ["AAAAAA", "BBBBBB"], // URI of each statistics page 
+    statsButtonsText:["Link to Statistics 1", "Link to Statistics 2"], // Description text of each button
+    selectedLanguage: "es_UY", // Selects language on /i18n folder 
+    selectedFlag: "img/flags/uy.png",
+    selectedRightLogo: "img/logo-salud-uy-small.png",
+    selectedLeftLogo: "",
+    copyrightText: "&copy; Salud.uy 2019",
+    bussinesLink: "https://Salud.uy"
 }
+
+
+// var config = { //IHMI
+//     serverUrl: "http://ihmi.termspace.com/api/snomed",
+//     edition: "en-edition",
+//     release: "v20180731",
+//     releaseName: "AMA Edition 2019-06-30",
+//     langRefset: ["900000000000509007"],
+//     statsButtons: [],
+//     statsButtonsText: [],
+//     selectedLanguage: "en_US",
+//     selectedFlag: "img/flags/us.png",
+//     selectedRightLogo: "",
+//     selectedLeftLogo:"img/ama-logo.png",
+//     copyrightText: "&copy; AMA 2019",
+//     bussinesLink: ""
+// }
